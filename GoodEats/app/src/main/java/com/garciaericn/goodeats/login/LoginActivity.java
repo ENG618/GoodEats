542c44e8b810fe5556198594fed52a53afb419bc
eng618 <eng618@fullsail.edu>
2014-11-10 00:27:52 -0500

    Initialized Google api client.

--- GoodEats/app/src/main/java/com/garciaericn/goodeats/login/LoginActivity.java
+++ GoodEats/app/src/main/java/com/garciaericn/goodeats/login/LoginActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.plus.Plus;
 
 /**
  * Full Sail University
@@ -13,16 +14,47 @@ import com.google.android.gms.common.api.GoogleApiClient;
  */
 public class LoginActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
 
+    /* Request code used to invoke sign in user interactions. */
+    private static final int RC_SIGN_IN = 0;
+
+    /* Client used to interact with Google APIs. */
+    private GoogleApiClient mGoogleApiClient;
+
+    /* A flag indicating that a PendingIntent is in progress and prevents
+     * us from starting further intents.
+     */
+    private boolean mIntentInProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API)
+                .addScope(Plus.SCOPE_PLUS_LOGIN)
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
     }
 
     /*
-        * Connection Callbacks
-        * */
-    @Override
+            * Connection Callbacks
+            * */
+     @Override
     public void onConnected(Bundle bundle) {
 
     }
