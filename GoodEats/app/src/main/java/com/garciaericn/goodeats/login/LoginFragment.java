90a7a427ca3748327582ba5a0b12d57554c1963c
eng618 <eng618@fullsail.edu>
2014-11-10 23:36:29 -0500

    Set login button to wide, and set up click listener.

--- GoodEats/app/src/main/java/com/garciaericn/goodeats/login/LoginFragment.java
+++ GoodEats/app/src/main/java/com/garciaericn/goodeats/login/LoginFragment.java
@@ -2,19 +2,20 @@ package com.garciaericn.goodeats.login;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.garciaericn.goodeats.R;
+import com.google.android.gms.common.SignInButton;
 
 /**
  * Full Sail University
  * Mobile Development BS
  * Created by ENG618-Mac on 11/10/14.
  */
-public class LoginFragment extends Fragment {
+public class LoginFragment extends Fragment implements View.OnClickListener{
 
     public static final String TAG = "com.garciaericn.goodeats.login.LoginFragment.TAG";
 
@@ -26,9 +27,32 @@ public class LoginFragment extends Fragment {
         return new LoginFragment();
     }
 
-    @Nullable
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        SignInButton signInButton = (SignInButton) view.findViewById(R.id.g_plus_login);
+        signInButton.setSize(SignInButton.SIZE_WIDE);
+        signInButton.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.g_plus_login:
+                Toast.makeText(getActivity(), "Sign in now", Toast.LENGTH_SHORT).show();
+                // TODO: Start login process here
+                break;
+            default:
+                // If default action is needed.
+                break;
+        }
     }
 }
