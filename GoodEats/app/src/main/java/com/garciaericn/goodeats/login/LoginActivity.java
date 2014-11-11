9a906907caff12bb292053202de61989571f9659
eng618 <eng618@fullsail.edu>
2014-11-10 23:58:59 -0500

    Migrated all Google api code to fragment.

--- GoodEats/app/src/main/java/com/garciaericn/goodeats/login/LoginActivity.java
+++ GoodEats/app/src/main/java/com/garciaericn/goodeats/login/LoginActivity.java
@@ -1,32 +1,16 @@
 package com.garciaericn.goodeats.login;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.content.IntentSender;
 import android.os.Bundle;
 
 import com.garciaericn.goodeats.R;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.plus.Plus;
 
 /**
  * Full Sail University
  * Mobile Development BS
  * Created by ENG618-Mac on 11/10/14.
  */
-public class LoginActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
-
-    /* Request code used to invoke sign in user interactions. */
-    private static final int RC_SIGN_IN = 0;
-
-    /* Client used to interact with Google APIs. */
-    private GoogleApiClient mGoogleApiClient;
-
-    /* A flag indicating that a PendingIntent is in progress and prevents
-     * us from starting further intents.
-     */
-    private boolean mIntentInProgress;
+public class LoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,68 +21,5 @@ public class LoginActivity extends Activity implements GoogleApiClient.Connectio
                 .beginTransaction()
                 .replace(R.id.login_container, LoginFragment.getInstance(), LoginFragment.TAG)
                 .commit();
-
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(Plus.API)
-                .addScope(Plus.SCOPE_PLUS_LOGIN)
-                .build();
-
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mGoogleApiClient.connect();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        if (mGoogleApiClient.isConnected()) {
-            mGoogleApiClient.disconnect();
-        }
-    }
-
-    /*
-    * Connection Callbacks
-    * */
-    @Override
-    public void onConnected(Bundle bundle) {
-        // Connection errors resolved
-        // Can now access Google APIs on behalf of the user
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        mGoogleApiClient.connect();
-    }
-
-    /*
-     *On Connections Failed Listener
-     * */
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        if (!mIntentInProgress && connectionResult.hasResolution()) {
-            try {
-                mIntentInProgress = true;
-                startIntentSenderForResult(connectionResult.getResolution().getIntentSender(), RC_SIGN_IN, null, 0, 0, 0);
-            } catch (IntentSender.SendIntentException e) {
-                mIntentInProgress = false;
-                mGoogleApiClient.connect();
-            }
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == RC_SIGN_IN) {
-            mIntentInProgress = false;
-
-            if (!mGoogleApiClient.isConnected()) {
-                mGoogleApiClient.connect();
-            }
-        }
     }
 }
--- GoodEats/app/src/main/java/com/garciaericn/goodeats/login/LoginFragment.java
+++ GoodEats/app/src/main/java/com/garciaericn/goodeats/login/LoginFragment.java
@@ -1,6 +1,8 @@
 package com.garciaericn.goodeats.login;
 
 import android.app.Fragment;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,16 +10,28 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.garciaericn.goodeats.R;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.plus.Plus;
 
 /**
  * Full Sail University
  * Mobile Development BS
  * Created by ENG618-Mac on 11/10/14.
  */
-public class LoginFragment extends Fragment implements View.OnClickListener{
+public class LoginFragment extends Fragment implements View.OnClickListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
 
     public static final String TAG = "com.garciaericn.goodeats.login.LoginFragment.TAG";
+
+    /* Request code used to invoke sign in user interactions. */
+    private static final int RC_SIGN_IN = 0;
+    /* Client used to interact with Google APIs. */
+    private GoogleApiClient mGoogleApiClient;
+    /* A flag indicating that a PendingIntent is in progress and prevents
+     * us from starting further intents.
+     */
+    private boolean mIntentInProgress;
 
     public LoginFragment() {
 
@@ -30,6 +44,14 @@ public class LoginFragment extends Fragment implements View.OnClickListener{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API)
+                .addScope(Plus.SCOPE_PLUS_LOGIN)
+                .build();
     }
 
     @Override
@@ -44,6 +66,20 @@ public class LoginFragment extends Fragment implements View.OnClickListener{
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.g_plus_login:
@@ -53,6 +89,43 @@ public class LoginFragment extends Fragment implements View.OnClickListener{
             default:
                 // If default action is needed.
                 break;
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        // Connection errors resolved
+        // Can now access Google APIs on behalf of the user
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        if (!mIntentInProgress && connectionResult.hasResolution()) {
+            try {
+                mIntentInProgress = true;
+                getActivity().startIntentSenderForResult(connectionResult.getResolution().getIntentSender(), RC_SIGN_IN, null, 0, 0, 0);
+
+
+            } catch (IntentSender.SendIntentException e) {
+                mIntentInProgress = false;
+                mGoogleApiClient.connect();
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RC_SIGN_IN) {
+            mIntentInProgress = false;
+
+            if (!mGoogleApiClient.isConnected()) {
+                mGoogleApiClient.connect();
+            }
         }
     }
 }
